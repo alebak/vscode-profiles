35d470a90ea1d1d70fda34c3c65429bd717584bd
Alebak <alebak@users.noreply.github.com>
2021-02-26 01:20:26 -0500

    First version of vscode.bat

---
+++ vscode.bat
@@ -0,0 +1,78 @@
+@ECHO OFF
+rem
+rem		Visual Studio Code Profile
+rem		Copyright (C) 2021  Alejandro Arroyave Valencia (@alebak)
+rem	
+rem		This program is free software: you can redistribute it and/or modify
+rem		it under the terms of the GNU General Public License as published by
+rem		the Free Software Foundation, either version 3 of the License, or
+rem		(at your option) any later version.
+rem	
+rem		This program is distributed in the hope that it will be useful,
+rem		but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem		GNU General Public License for more details.
+rem	
+rem		You should have received a copy of the GNU General Public License
+rem		along with this program.  If not, see <https://www.gnu.org/licenses/>.
+rem
+
+SET n=^&echo.
+SET "t=    "
+
+IF "%~1" == "" GOTO default
+IF "%~1" == "--help" GOTO help
+IF "%~1" == "-h" GOTO help
+IF "%~1" == "--go" GOTO go
+IF "%~1" == "--js" GOTO javascript
+IF "%~1" == "--php" GOTO php
+IF "%~1" == "--python" GOTO python
+IF "%~1" == "--ts" GOTO typescript
+
+ECHO The '%1' profile for Visual Studio Code does not exist.
+GOTO exit
+
+:go
+	ECHO Visual Studio Code for Go
+	GOTO loadprofile
+
+:javascript
+	ECHO Visual Studio Code for JavaScript
+	GOTO loadprofile
+
+:php
+	ECHO Visual Studio Code for PHP
+	GOTO loadprofile
+	
+:python
+	ECHO Visual Studio Code for Python
+	GOTO loadprofile
+
+:typescript
+	ECHO Visual Studio Code for TypeScript
+	GOTO loadprofile
+
+
+:loadprofile
+	code --extensions-dir %USERPROFILE%/.vscode-profiles/%~1/exts --user-data-dir %USERPROFILE%/.vscode-profiles/%~1/data
+	GOTO exit
+	
+:default 
+	code
+	GOTO exit
+	
+:help
+	ECHO Visual Studio Code Profile 1.0.0 %n%
+	ECHO Usage: vscode.bat [options] %n%
+	ECHO If the profile directory does not exist... Relax! Visual Studio Profile creates it for you. &
+	ECHO If no option is passed, Visual Studio Code Profile runs Visual Studio with the default user profile. %n%
+	ECHO Options &
+	ECHO %t%--go     %t%%t%Go profile.
+	ECHO %t%--js     %t%%t%JavaScript profile.
+	ECHO %t%--php    %t%%t%PHP profile.
+	ECHO %t%--python %t%%t%Python profile.
+	ECHO %t%--ts     %t%%t%TypeScript profile.%n%
+	ECHO %t%-h --help%t%%t%Print usage.
+	GOTO exit	
+
+:exit
